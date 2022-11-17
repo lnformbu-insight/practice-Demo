ffe21b7bdf50c2b44dd8c0ca4c6d1d9c1d3caf15
Lenon nformbui <lenon.nformbui@insight.com>
2022-11-17 15:48:45 -0500

    added storage accounts

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 #An Azure resource group (azurerm_resource_group) that consumes both the resource_group_name and location variables.
-resource "azurerm_resource_group" "ARG-GPS" {
+resource "azurerm_resource_group" "GPS" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = "main-resources"
-  location = "eastus2"
-}
+ resource "azurerm_resource_group" "RGNM" {
+   name     = "example-resources"
+   location = "eastus2"
+ }
 
-resource "azurerm_storage_account" "failure" {
+resource "azurerm_storage_account" "STAC" {
   name                     = "teststroage"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
+  resource_group_name      = azurerm_resource_group.RGNM.name
+  location                 = azurerm_resource_group.RGNM.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
