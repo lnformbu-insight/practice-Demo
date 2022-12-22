43d4df4b6086cd75aa9f5e255089658ca0d29576
Lenon nformbui <lenon.nformbui@insight.com>
2022-12-22 14:13:05 -0500

    completed task AB#155

--- main.tf
+++ main.tf
@@ -1,18 +1,37 @@
-#An Azure resource group (azurerm_resource_group) that consumes both the resource_group_name and location variables.
-resource "azurerm_resource_group" "GPS" {
-  name     = var.resource_group_name
+resource "azurerm_resource_group" "resgrp" {
+  for_each = toset(var.resource_group_name)
+  name     = each.value
   location = var.location
 }
-
-
-resource "azurerm_storage_account" "STAC" {
-  name                     = "lenonstorage"
-  resource_group_name      = azurerm_resource_group.GPS.name
-  location                 = azurerm_resource_group.GPS.location
+# Deploy an SQL Database iin each resource created above (dev,uat,prod)
+resource "azurerm_storage_account" "stgact" {
+  for_each                 = azurerm_resource_group.resgrp
+  name                     = var.storage_account
+  resource_group_name      = each.value.name
+  location                 = var.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = local.tags
+  account_replication_type = "LRS"
 }
 
 
+resource "azurerm_mssql_server" "msqlsrv" {
+  for_each = azurerm_resource_group.resgrp
+  name                         = var.mssql_server
+  resource_group_name      = each.value.name
+  location                 = var.location
+  version                      = "12.0"
+  administrator_login          = var.msqlsrv_username
+  administrator_login_password = var.msqlsrv_password
+}
+
+resource "azurerm_mssql_database" "msqldatabase" {
+  for_each = azurerm_resource_group.resgrp
+  name           = var.mssql_database
+  server_id      = each.value.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "BC_Gen5_2"
+  zone_redundant = true
+} 
