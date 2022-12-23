4583216361b3a0931789f8c0381a1dff859d3fb8
Lenon nformbui <lenon.nformbui@insight.com>
2022-12-22 19:12:57 -0500

    storage account added

--- main.tf
+++ main.tf
@@ -13,39 +13,33 @@ resource "azurerm_storage_account" "stract" {
   account_replication_type = "LRS"
 }
 
-
-
-
-/*
-resource "azurerm_mssql_server" "example" {
-  name                         = "example-sqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+resource "azurerm_mssql_server" "mslsvr" {
+  for_each                     = azurerm_resource_group.resgrp
+  name                         = "${each.key}${var.mssql_server}"
+  resource_group_name          = each.value.name
+  location                     = var.location
   version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  administrator_login          = var.msqlsrv_username
+  administrator_login_password = var.msqlsrv_password
 }
 
 resource "azurerm_mssql_database" "test" {
-  name           = "acctest-db-d"
-  server_id      = azurerm_mssql_server.example.id
+  for_each       = azurerm_resource_group.resgrp
+  name           = "${each.key}${var.mssql_database}"
+  server_id      = azurerm_mssql_server.mslsvr[each.key].id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
   max_size_gb    = 4
   read_scale     = true
   sku_name       = "S0"
   zone_redundant = true
-
-  tags = {
-    foo = "bar"
-  }
 }
 
 
 
 
 
-
+/*
 
 ###########################################################################
 ###########################################################################
