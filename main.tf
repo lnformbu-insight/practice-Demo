00e230f6038d2cb084b03934e47e0ada310e70af
Lenon nformbui <lenon.nformbui@insight.com>
2022-12-22 19:26:36 -0500

    updated storage

--- main.tf
+++ main.tf
@@ -29,7 +29,6 @@ resource "azurerm_mssql_database" "test" {
   server_id      = azurerm_mssql_server.mslsvr[each.key].id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
   read_scale     = true
   sku_name       = "S0"
   zone_redundant = true
