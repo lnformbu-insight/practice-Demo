79a44047d585ae1c440b1cb79b197a942ac1d512
Lenon nformbui <lenon.nformbui@insight.com>
2022-12-28 14:55:46 -0500

    test

--- main.tf
+++ main.tf
@@ -28,10 +28,10 @@ resource "azurerm_mssql_server" "mssqlsrv" {
 resource "azurerm_mssql_database" "mssqldb" {
   name           = var.mssql_database
   server_id      = azurerm_mssql_server.mssqlsrv.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  read_scale     = true
-  sku_name       = "S0"
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  sku_name     = "S0"
+  max_size_gb  = 2
 
   tags = {
     environment = "uat"
